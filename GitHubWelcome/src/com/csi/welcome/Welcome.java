d2ca6f185e95cf754c3fe2ce98c4bd6e8534811e
parabshubham787 <parabshubham787@gmail.com>
2021-11-25 19:01:33 +0530

    changes successfully

--- GitHubWelcome/src/com/csi/welcome/Welcome.java
+++ GitHubWelcome/src/com/csi/welcome/Welcome.java
@@ -1,9 +1,9 @@
 package com.csi.welcome;
-
+//changes by shubham
 public class Welcome 
 {
 	public static void main(String[] args) {
 		
-		System.out.println("Welcome to GitHub");
+		System.out.println("Welcome to GitHub Project");
 	}
 }
